8f4a5eb7d7cce79cac3fd447ca365a4cb1409e95
simon.schmid <schms27@gmail.com>
2023-07-20 10:34:11 +0200

    initial file download test

---
+++ FileDownload/FileDownload/Controllers/FileController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace FileDownload.Controllers
+{
+    public class FileController : Controller
+    {
+        public ActionResult DownloadFile()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(@"c:\audio.log");
+            string fileName = "audio.log";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+    }
+}
